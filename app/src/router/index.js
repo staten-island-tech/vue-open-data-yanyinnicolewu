5fc5aadca145867ade8268c883b53cca4015a465
yanyinnicolewu <nicolewu288@gmail.com>
2026-03-17 12:32:34 -0400

    svd

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import BabyName from '@/views/BabyName.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'babyname',
+      component: BabyName,
+    },
+  ],
 })
 
 export default router
---
+++ app/src/views/BabyName.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <BabyCard v-for="(baby, index) in babyname" :key="baby.name" :babyname="baby" :id="index + 1" />
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+import BabyCard from '../components/BabyCard.vue'
+</script>
+
+<style scoped></style>
